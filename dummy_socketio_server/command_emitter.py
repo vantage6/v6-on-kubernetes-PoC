d22ff8a3080be92e5b179f5672664a9bfae63fca
Cunliang Geng <c.geng@esciencecenter.nl>
2024-02-17 21:09:52 +0100

    use multiarch image

--- dummy_socketio_server/command_emitter.py
+++ dummy_socketio_server/command_emitter.py
@@ -8,7 +8,7 @@ sio.connect('http://192.168.178.185:5000')
 run_cmd_payload = {
     "run_id": 33333312,
     "task_info": {"arg1":"/app/input/csv/default","arg2":"Age","arg3":"/app/output/avg.txt"},
-    "image": "hectorcadavid/avg-alg-x86",
+    "image": "clgeng/v6_average_algorithm",
     "docker_input": "input1",
     "tmp_vol_name": "example-tmp-vol",
     "token": "example_token",
