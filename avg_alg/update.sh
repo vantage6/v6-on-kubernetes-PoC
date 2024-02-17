77e1a87705b6227cd5a64cb7a37eee102258d4c3
Cunliang Geng <c.geng@esciencecenter.nl>
2024-02-17 21:07:25 +0100

    add commands to build multiarch images

--- avg_alg/update.sh
+++ avg_alg/update.sh
@@ -1,2 +1,13 @@
+# Build x86 image
+
 docker build . -t hectorcadavid/avg-alg-x86
 docker push  hectorcadavid/avg-alg-x86
+
+
+# Build multiarch image (https://www.docker.com/blog/multi-arch-images/)
+
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+docker buildx inspect --bootstrap
+
+docker buildx build --platform linux/arm64,linux/amd64,linux/amd64/v2,linux/arm/v7,linux/arm/v6 -t clgeng/v6_average_algorithm --push .
