52792425457d5ff57f0f530233cf62cf5e3b54a8
Hector Cadavid <you@example.com>
2024-07-25 20:00:42 +0000

    Simple CLI client for requesting a task using a V6 test algorithm

---
+++ v6-client/config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# 
+server_url = ""                                         
+server_port = 443 
+server_api = "/api"
+
+username = "kube-a-org-admin"
+password = ""
+
+organization_key = "" # This can be empty if you do not want to set up encryption
---
+++ v6-client/rclient.py
@@ -0,0 +1,27 @@
+from vantage6.client import UserClient as Client
+
+import config
+
+# Initialize the client object, and run the authentication
+client = Client(config.server_url, config.server_port, config.server_api, log_level='debug')
+client.authenticate(config.username, config.password)
+
+# Optional: setup the encryption, if you have an organization_key
+client.setup_encryption(None)
+
+input_ = {
+    'method': 'partial',
+    'kwargs': {"colname":"Age"}
+}
+
+average_task = client.task.create(
+   collaboration=1,
+   organizations=[2],
+   name="poc_model",
+   image="hcadavidescience/poc_model_training",
+   description='',
+   input_=input_,
+   databases=[
+      {'label': 'default'}
+   ]
+)
